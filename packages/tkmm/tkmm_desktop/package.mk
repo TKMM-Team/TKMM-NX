720245be99163848ce4f5ee500ff0beef3023910
MasterBubbles <guiguy046@hotmail.fr>
2024-09-14 17:39:28 +0200

    Remove unnecessary files

--- packages/tkmm/tkmm_desktop/package.mk
+++ packages/tkmm/tkmm_desktop/package.mk
@@ -5,7 +5,6 @@ PKG_SITE="https://tkmm.org/"
 PKG_URL="https://github.com/TKMM-Team/Tkmm/releases/download/${PKG_VERSION}/TKMM-linux-arm64.zip"
 PKG_DEPENDS_TARGET="dotnet-sdk libICE libSM icu tkmm_assets fontconfig"
 PKG_LONGDESC="TKMM is an easy to use mod manager and file merger for Tears of the Kingdom"
-PKG_LR_UPDATE_TAG="yes"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/bin
