3ccced2a84c8474103341d339fbbe3c6f6543280
MasterBubbles <guiguy046@hotmail.fr>
2024-09-14 17:25:08 +0200

    Implement TKMM package

---
+++ packages/tkmm/tkmm/package.mk
@@ -0,0 +1,19 @@
+PKG_NAME="tkmm"
+PKG_VERSION="v1.0.0-beta11"
+PKG_LICENSE="GPLv3"
+PKG_SITE="https://tkmm.org/"
+PKG_URL="https://github.com/TKMM-Team/Tkmm/releases/download/${PKG_VERSION}/TKMM-linux-arm64.zip"
+PKG_DEPENDS_TARGET="dotnet-sdk libICE libSM icu tkmm_assets fontconfig"
+PKG_LONGDESC="TKMM is an easy to use mod manager and file merger for Tears of the Kingdom"
+PKG_LR_UPDATE_TAG="yes"
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/bin
+    cp -v ${PKG_BUILD}/tkmm ${INSTALL}/usr/bin
+    cp -v ${PKG_DIR}/scripts/tkmm-*.sh ${INSTALL}/usr/bin
+
+post_install() {
+  enable_service tkmm.target
+  enable_service tkmm-autostart.service
+  enable_service tkmm.service
+}
---
+++ packages/tkmm/tkmm/scripts/tkmm-reboot.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sleep 3
+systemctl stop tkmm
+sleep 1
+shutdown -r now
