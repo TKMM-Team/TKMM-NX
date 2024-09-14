6e17aed460ab93536adbcc54d3aa7807d867293e
MasterBubbles <guiguy046@hotmail.fr>
2024-09-14 18:00:35 +0200

    Change toolchain to manual

--- packages/tkmm/tkmm/package.mk
+++ packages/tkmm/tkmm/package.mk
@@ -5,6 +5,7 @@ PKG_SITE="https://tkmm.org/"
 PKG_URL="https://github.com/TKMM-Team/Tkmm/releases/download/${PKG_VERSION}/TKMM-linux-arm64.zip"
 PKG_DEPENDS_TARGET="dotnet-sdk libICE libSM icu tkmm_assets fontconfig"
 PKG_LONGDESC="TKMM is an easy to use mod manager and file merger for Tears of the Kingdom"
+PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/bin
