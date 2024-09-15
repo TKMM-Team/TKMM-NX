8e95816d9e141452283b0eaffed347a18070d2ab
bubbles <guiguy046@hotmail.fr>
2024-09-15 17:19:33 +0200

    clean up TKMM package

--- packages/tkmm/tkmm/package.mk
+++ packages/tkmm/tkmm/package.mk
@@ -1,16 +1,12 @@
 PKG_NAME="tkmm"
-PKG_VERSION="v1.0.0-beta11"
 PKG_LICENSE="MIT"
 PKG_SITE="https://tkmm.org/"
-PKG_URL="https://github.com/TKMM-Team/Tkmm/releases/download/${PKG_VERSION}/TKMM-linux-arm64.zip"
-PKG_SOURCE_NAME="TKMM-linux-arm64.zip"
 PKG_DEPENDS_TARGET="dotnet-sdk libICE libSM icu tkmm_assets fontconfig"
 PKG_LONGDESC="TKMM is an easy to use mod manager and file merger for Tears of the Kingdom"
 PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/bin
-#    cp -v ${PKG_BUILD}/* ${INSTALL}/usr/bin
     cp -v ${PKG_DIR}/scripts/tkmm-*.sh ${INSTALL}/usr/bin
 }
 
