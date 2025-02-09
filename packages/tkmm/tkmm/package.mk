0b544acd84a54fc7f9a5f5871dbdc348b8ffe9ee
MasterBubbles <guiguy046@hotmail.fr>
2025-02-09 08:31:12 +0100

    support tag name with leading v

--- packages/tkmm/tkmm/package.mk
+++ packages/tkmm/tkmm/package.mk
@@ -1,8 +1,7 @@
 PKG_NAME="tkmm"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/TKMM-Team/Tkmm"
-#PKG_VERSION="v${TKMM_VERSION}"
-PKG_VERSION="9f3064aa4c2441264579daf43b08a49c5f69ecd3"
+PKG_VERSION="${TKMM_VERSION}"
 PKG_DEPENDS_TARGET="libICE libSM icu tkmm_assets fontconfig xf86-input-joystick 7-zip"
 PKG_LONGDESC="TKMM is an easy to use mod manager and file merger for Tears of the Kingdom"
 PKG_URL="${PKG_SITE}.git"
@@ -11,12 +10,12 @@ PKG_TOOLCHAIN="manual"
 PKG_BUILD_FLAGS=" -c Release \
                   -r linux-arm64 \
                   --sc false \
-                  --version-suffix v${TKMM_VERSION} \
+                  --version-suffix ${TKMM_VERSION} \
                   -o ${INSTALL}/tkmm/tmp \
                   -p:DebugType=embedded \
                   -p:IncludeNativeLibrariesForSelfExtract=true \
                   -p:PublishSingleFile=true \
-                  -p:Version=${TKMM_VERSION} \
+                  -p:Version=${TKMM_VERSION#v} \
                   -p:DefineConstants=SWITCH%3BTRACE%3BTARGET_NX%3BRELEASE"
 
 pre_make_target() {
