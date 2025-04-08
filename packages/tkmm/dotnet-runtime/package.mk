7ec34a0c833ea58cf051e25c31dffd841134b8ea
LordBubbles <48891568+LordBubblesDev@users.noreply.github.com>
2025-04-08 19:29:15 +0200

    update dotnet

--- packages/tkmm/dotnet-runtime/package.mk
+++ packages/tkmm/dotnet-runtime/package.mk
@@ -2,13 +2,13 @@
 # Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="dotnet-runtime"
-PKG_VERSION="9.0.2"
+PKG_VERSION="9.0.3"
 PKG_LICENSE="MIT"
 PKG_SITE="https://dotnet.microsoft.com/"
 PKG_LONGDESC=".NET Runtime is a set of libraries to run .NET applications."
 PKG_TOOLCHAIN="manual"
 PKG_AUTORECONF="no"
-PKG_SHA256="63f29ad13759d3bd7e356f148f0f0fcedc9c17f219a519023ceace53d02df3b8"
+PKG_SHA256="34f948977e57c8a88b8c7eb8775a89fe3e48357044bd6f7cf6862631ad3255c1"
 PKG_URL="https://builds.dotnet.microsoft.com/dotnet/Runtime/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}-linux-arm64.tar.gz"
 PKG_SOURCE_NAME="${PKG_NAME}-${PKG_VERSION}-${ARCH}.tar.gz"
 
--- packages/tkmm/dotnet-sdk/package.mk
+++ packages/tkmm/dotnet-sdk/package.mk
@@ -2,13 +2,13 @@
 # Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="dotnet-sdk"
-PKG_VERSION="9.0.200"
+PKG_VERSION="9.0.202"
 PKG_LICENSE="MIT"
 PKG_SITE="https://dotnet.microsoft.com/"
 PKG_LONGDESC=".NET SDK is a set of libraries and tools that developers use to create .NET applications and libraries."
 PKG_TOOLCHAIN="manual"
 PKG_AUTORECONF="no"
-PKG_SHA256="3266045a9b8a5362dca0e148a263385e9ae94920d6f2f56802ad2d5996988bab"
+PKG_SHA256="e38c65156588446f4686b50607996708a4648f9075efe12d73857473f9e10136"
 PKG_URL="https://builds.dotnet.microsoft.com/dotnet/Sdk/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}-linux-arm64.tar.gz"
 PKG_SOURCE_NAME="${PKG_NAME}-${PKG_VERSION}-${ARCH}.tar.gz"
 
