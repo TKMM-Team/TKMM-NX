61ce2fc3abf213b4c63771aa6c0bfe92dfe7513d
LordBubbles <48891568+LordBubblesDev@users.noreply.github.com>
2025-05-31 17:53:14 +0200

    add libXcursor to TKMM dependencies

--- packages/tkmm/tkmm/package.mk
+++ packages/tkmm/tkmm/package.mk
@@ -1,7 +1,7 @@
 PKG_NAME="tkmm"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/TKMM-Team/Tkmm"
-PKG_DEPENDS_TARGET="libICE libSM icu fontconfig xf86-input-joystick ffmpegx 7-zip"
+PKG_DEPENDS_TARGET="libICE libSM libXcursor icu fontconfig xf86-input-joystick ffmpegx 7-zip"
 PKG_LONGDESC="TKMM is an easy to use mod manager and file merger for Tears of the Kingdom"
 PKG_URL="${PKG_SITE}.git"
 PKG_TOOLCHAIN="manual"
