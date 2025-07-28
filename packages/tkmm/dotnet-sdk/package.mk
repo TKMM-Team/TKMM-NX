2575527fe0f911ba899ba689360b8f673cac30e6
LordBubbles <48891568+LordBubblesDev@users.noreply.github.com>
2025-07-28 05:12:06 +0200

    Update .NET Runtime/SDK

--- packages/tkmm/dotnet-sdk/package.mk
+++ packages/tkmm/dotnet-sdk/package.mk
@@ -2,13 +2,13 @@
 # Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="dotnet-sdk"
-PKG_VERSION="9.0.300"
+PKG_VERSION="9.0.303"
 PKG_LICENSE="MIT"
 PKG_SITE="https://dotnet.microsoft.com/"
 PKG_LONGDESC=".NET SDK is a set of libraries and tools that developers use to create .NET applications and libraries."
 PKG_TOOLCHAIN="manual"
 PKG_AUTORECONF="no"
-PKG_SHA256="ea626d17422adfd8d1de4fd53e0696004f2c60835352035dcc0bd95569d872b9"
+PKG_SHA256="4dc5fa026457b7c90dae047ffe5846bc7b5a7721890f5a7d76a97a232ad92f02"
 PKG_URL="https://builds.dotnet.microsoft.com/dotnet/Sdk/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}-linux-arm64.tar.gz"
 PKG_SOURCE_NAME="${PKG_NAME}-${PKG_VERSION}-${ARCH}.tar.gz"
 
