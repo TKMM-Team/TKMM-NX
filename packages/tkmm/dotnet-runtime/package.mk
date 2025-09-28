5ba4494c749da088dea9b897c1de4c0dc9cf3a9f
GitHub Action <action@github.com>
2025-09-28 16:52:23 +0000

    Update .NET packages to latest versions
    - dotnet-runtime: Updated to 9.0.9
    - dotnet-sdk: Updated to 9.0.305
    
    This update was automatically generated by the GitHub Actions workflow.

--- packages/tkmm/dotnet-runtime/package.mk
+++ packages/tkmm/dotnet-runtime/package.mk
@@ -2,13 +2,13 @@
 # Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="dotnet-runtime"
-PKG_VERSION="9.0.7"
+PKG_VERSION="9.0.9"
 PKG_LICENSE="MIT"
 PKG_SITE="https://dotnet.microsoft.com/"
 PKG_LONGDESC=".NET Runtime is a set of libraries to run .NET applications."
 PKG_TOOLCHAIN="manual"
 PKG_AUTORECONF="no"
-PKG_SHA256="4708c57d7f8a0594a9d970528505c543ab05d8a75659604e1085133580a4d35e"
+PKG_SHA256="2b71d6caffe20658e025cbf4459f70f2704c943e515b96ebb969a282619c5b51"
 PKG_URL="https://builds.dotnet.microsoft.com/dotnet/Runtime/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}-linux-arm64.tar.gz"
 PKG_SOURCE_NAME="${PKG_NAME}-${PKG_VERSION}-${ARCH}.tar.gz"
 
