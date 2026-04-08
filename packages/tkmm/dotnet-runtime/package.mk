97a9b19ddca2bb181db95d9735e3abd332c5a26a
LordBubbles <48891568+LordBubblesDev@users.noreply.github.com>
2026-04-08 04:39:15 +0200

    Revert "Temp fix for dotnet runtime"
    This reverts commit febdaf37724901502771901adba90d93b6b324e9.

--- packages/tkmm/dotnet-runtime/package.mk
+++ packages/tkmm/dotnet-runtime/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0
 # Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
 
-PKG_NAME="dotnet-runtime-10"
+PKG_NAME="dotnet-runtime"
 PKG_VERSION="10.0.5"
 PKG_LICENSE="MIT"
 PKG_SITE="https://dotnet.microsoft.com/"
@@ -9,7 +9,7 @@ PKG_LONGDESC=".NET Runtime is a set of libraries to run .NET applications."
 PKG_TOOLCHAIN="manual"
 PKG_AUTORECONF="no"
 PKG_SHA256="09c27bcdfe3f4ef5716499abeff573ec3686051cc5fef76760057f16264227e3"
-PKG_URL="https://builds.dotnet.microsoft.com/dotnet/Runtime/${PKG_VERSION}/dotnet-runtime-${PKG_VERSION}-linux-arm64.tar.gz"
+PKG_URL="https://builds.dotnet.microsoft.com/dotnet/Runtime/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}-linux-arm64.tar.gz"
 PKG_SOURCE_NAME="${PKG_NAME}-${PKG_VERSION}-${ARCH}.tar.gz"
 
 makeinstall_target() {
